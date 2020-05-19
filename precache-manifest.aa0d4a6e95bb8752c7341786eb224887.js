6bcb45a9976f74f04c24368082b013ddd560c3d9
youssefsiam38 <youssef1yasser38@gmail.com>
2020-05-19 08:18:22 +0200

    Updates

---
+++ precache-manifest.aa0d4a6e95bb8752c7341786eb224887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85a8169ddb429d0a37eac7b4ad4a35d",
+    "url": "/tictoc/index.html"
+  },
+  {
+    "revision": "8f744b6027bf33805c24",
+    "url": "/tictoc/static/css/main.a2e530d1.chunk.css"
+  },
+  {
+    "revision": "b97c0ea54b7b50c66b43",
+    "url": "/tictoc/static/js/2.b2246ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictoc/static/js/2.b2246ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f744b6027bf33805c24",
+    "url": "/tictoc/static/js/main.77d2331f.chunk.js"
+  },
+  {
+    "revision": "f82a3c0cac7b814cae2a",
+    "url": "/tictoc/static/js/runtime-main.aadd6390.js"
+  }
+]);
